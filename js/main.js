11031c44eff23390a8ffa96d6058dfb4f514efa4
MurodillaErgashev03 <ergashevmurodilla27@gmail.com>
2023-02-16 00:50:38 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -15,32 +15,42 @@ elInput.addEventListener("change" , () => {
   fetch("https://restcountries.com/v3.1/name/" + elInput.value)
   .then((res) => res.json() )
   .then((data) => {
-    console.log(data)
+    
     Map.href = data[0].maps.googleMaps;
+  
+ 
+    console.log(data[0].name.common)
 
-    let li =  document.createElement("li");
-     ul.textContent = "";
+    if(elInput.value.toLocaleLowerCase() === data[0].name.common.toLocaleLowerCase()){
 
-    li.innerHTML = `
-    <li class="list-group-item">
-    <img class="img" src="${data[0].flags.png}">
-      <p class="capital">
-         <span>Capital : </span>${data[0].capital}
-      </p>
-      <p class="area">
-         <span>Area : </span> ${data[0].area} 
-      </p>
-      <p class="popilation">
-        <span>Popilation : </span> ${data[0].population}
-      </p>
-      <p>
-       <span>Region : </span> ${data[0].region} 
-      </p>
-  </li>
-    `
-    ul.appendChild(li)
-     
-     elInput.value = '';
+      let li =  document.createElement("li");
+      ul.textContent = "";
+
+      li.innerHTML = `
+      <li class="list-group-item">
+      <img class="img" src="${data[0].flags.png}">
+      <p class="name">
+      <span>Name : </span> ${data[0].name.common} 
+   </p>
+        <p class="capital">
+           <span>Capital : </span>${data[0].capital}
+        </p>
+        <p class="area">
+           <span>Area : </span> ${data[0].area} 
+        </p>
+        <p class="popilation">
+          <span>Popilation : </span> ${data[0].population}
+        </p>
+        <p>
+         <span>Region : </span> ${data[0].region} 
+        </p>
+    </li>
+      `
+      ul.appendChild(li)
+       
+       elInput.value = '';
+    }
+
   });
     
 
